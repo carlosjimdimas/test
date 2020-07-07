3f701e983410b36059ea54c8a7784ae746dd194f
carlosjimdimas <carlos123120@gmail.com>
2020-07-07 16:37:24 -0500

    Funcionalidad de conexion a Base de Datos en diferentes esquemas Completada

--- src/main/java/com/test/evaluacion/entity/Identificacion.java
+++ src/main/java/com/test/evaluacion/entity/Identificacion.java
@@ -2,17 +2,26 @@ package com.test.evaluacion.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 
-@Entity (name = "identificacion")
+@Entity
 @Data
+@Table(name ="Identificacion", schema = "identificacion_schema")
 public class Identificacion {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column(name = "id_identificacion")
+    private Integer id;
     @NotNull
+    @Column(name = "identificacion_name")
     private String identificacionName;
     @NotNull
-    private int identificacion;
+    @Column(name = "decription")
+    private int descripcion;
+
+    @Column(name = "created_at")
     private Date createdAt;
 }
